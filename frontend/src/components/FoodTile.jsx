2ba9033904a9521040bd15f1824780443fec244a
Basim Hilal V <basimhilalofficial@gmail.com>
2024-11-23 20:16:15 +0530

    FoodTile configure

--- frontend/src/components/FoodTile.jsx
+++ frontend/src/components/FoodTile.jsx
@@ -1,11 +1,36 @@
 import React from 'react'
+import './FoodTile.css'
+import { food_list } from '../assets/assets'
+const FoodTile = ({ category }) => {
+    return (
+        <div className='food-display' id='food-display'>
+            <h2>Top dishes near you</h2>
+            <div className="food-display-list">
+                {food_list.map((item, index) => {
+                    if (category === "All" || category === item.category) {
 
-const FoodTile = () => {
-  return (
-    <div>
-      Foods
-    </div>
-  )
+                        return (
+                            <div>
+                                <div className='food-item'>
+                                    <div className="food-item-img-container">
+                                        <img src={item.image} alt="" className="food-item-image" />
+                                    </div>
+                                    <div className="food-item-info">
+                                        <div className="food-item-name-rating">
+                                            <p>{item.name}</p>
+                                        </div>
+                                        <p className="food-item-desc">{item.description}</p>
+                                        <p className="food-item-price">${item.price}</p>
+                                    </div>
+                                </div>
+                            </div>
+                        )
+                        //<FoodItem key={index} id={item._id} name={item.name} description={item.description} price={item.price} image={item.image} />
+                    }
+                })}
+            </div>
+        </div>
+    )
 }
 
 export default FoodTile
